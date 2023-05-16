ca4751dadc03f22d0bdd144841774d6ca6827486
kazimahin68 <kazimahin68@gmail.com>
2023-05-17 02:00:20 +0600

    specific service and booking added

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-
-console.log(process.env.DB_USER)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j52gohc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,6 +24,8 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const serviceCollection = client.db('carDoctor').collection('services')
+        // New Collection
+        const bookingCollection = client.db('carDoctor').collection('bookings')
         // Get services data
         app.get('/services', async (req, res) => {
             const query = {};
@@ -35,6 +35,31 @@ async function run() {
             const cursor = serviceCollection.find(query, options);
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const options = {
+                projection: { title: 1, price: 1, img: 1 }
+            }
+            const result = await serviceCollection.findOne(query, options)
+            res.send(result);
+        })
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        app.get('/bookings', async(req, res) =>{
+            let query = {};
+            if(req.query?.email){
+                query = {email : req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result);
         })
 
 
